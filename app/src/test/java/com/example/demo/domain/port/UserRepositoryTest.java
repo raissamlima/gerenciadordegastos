b680c9d7d4c6160fe7f940985c06623a0e0e2b36
luksquitos <lucasmartinscaet@hotmail.com>
2026-05-13 19:32:23 -0300

    feat: creating a docker-compose with postgres to project
    feat: adding configuration properties to application
    
    feat: adding jpa and postgres dependencies to pom.xml

---
+++ app/src/test/java/com/example/demo/domain/port/UserRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.domain.port;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+}
