65e5341e4b4966f37526c03395e42444f18c50a6
luksquitos <lucasmartinscaet@hotmail.com>
2026-05-19 19:32:53 -0300

    chore: using lombok allargsconstructor to avoid boilerplate

--- app/src/main/java/com/example/demo/domain/model/Transaction.java
+++ app/src/main/java/com/example/demo/domain/model/Transaction.java
@@ -1,23 +1,17 @@
 package com.example.demo.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Getter
+@AllArgsConstructor
 public class Transaction {
     private String uuid;
     private String senderId;
     private String receiverId;
     private BigDecimal amount;
     private LocalDateTime createdAt;
-
-    public Transaction(String uuid, String senderId, String receiverId, BigDecimal amount, LocalDateTime createdAt){
-        this.uuid = uuid;
-        this.senderId = senderId;
-        this.receiverId = receiverId;
-        this.amount = amount;
-        this.createdAt = createdAt;
-    }
 }
