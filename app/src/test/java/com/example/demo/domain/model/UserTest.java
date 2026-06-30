f496008051b6eaeeefda583090adf1b952e115ad
luksquitos <lucasmartinscaet@hotmail.com>
2026-06-30 20:11:02 -0300

    feat: writing tests for use cases
    chore: renaming tests methods to english

--- app/src/test/java/com/example/demo/domain/model/UserTest.java
+++ app/src/test/java/com/example/demo/domain/model/UserTest.java
@@ -11,20 +11,20 @@ import static org.assertj.core.api.Assertions.*;
 class UserTest {
 
     @Test
-    void debit_deveDiminuirSaldo_quandoSaldoSuficiente() {
+    void debit_shouldDecreaseBalance_whenBalanceIsSufficient() {
         User user = new User("1", "Lucas Martins", "123.456.789-10", "SP", new BigDecimal("1000.00"));
         user.debit(new BigDecimal("700.00"));
         assertThat(user.getBalance()).isEqualTo(new BigDecimal("300.00"));
     }
 
     @Test
-    void debit_deveLancarInsufficientBalanceException_quandoSaldoInsuficiente(){
+    void debit_shouldThrowInsufficientBalanceException_whenBalanceIsInsufficient(){
         User user = new User("1", "Lucas Martins", "123.456.789.10", "SP", new BigDecimal("1000.00"));
         assertThatThrownBy(() -> user.debit(new BigDecimal("2000.00"))).isInstanceOf(InsufficientBalanceException.class);
     }
 
     @Test
-    void credit_deveAumentarSaldo() {
+    void credit_shouldIncreaseBalance() {
         User user = new User("1", "Lucas Martins", "123.456.789-10", "SP", new BigDecimal("1000.00"));
         user.credit(new BigDecimal("700.00"));
         assertThat(user.getBalance()).isEqualTo(new BigDecimal("1700.00"));
